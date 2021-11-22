3acb0d6c0e0556d00f083ea5629ca1d63a2bb200
nahidiac <nahidiac@gmail.com>
2021-11-22 20:55:36 +0600

    solved 80A

---
+++ A2ojLadder1/80A/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+
+namespace _80A
+{
+    class Program
+    {
+        static bool[] isPrime; 
+        static void Main(string[] args)
+        {
+            sieve();
+
+            var input = (Console.ReadLine()).Split(' ');
+            var number1 = int.Parse(input[0]);
+            var number2 = int.Parse(input[1]);
+
+            if(!isPrime[number2])
+            {
+                Console.WriteLine("NO");
+            }
+            else
+            {
+                var nextPrime = 0;
+
+                for (int i = number1 + 1; i <= 50; i++)
+                {
+                    if (isPrime[i])
+                    {
+                        nextPrime = i;
+                        break;
+                    }
+                }
+
+                if (number2 == nextPrime)
+                {
+                    Console.WriteLine("YES");
+                }
+                else
+                {
+                    Console.WriteLine("NO");
+                }
+            }
+            
+        }
+
+        static void sieve()
+        {
+            isPrime = new bool[51];
+
+            for(int i = 2; i <= 50; i++)
+            {
+                isPrime[i] = true;
+            }
+
+            for(int i = 4; i <= 50; i += 2)
+            {
+                isPrime[i] = false; 
+            }
+
+            for(int i = 3; i*i <= 50; i += 2)
+            {
+                for(int j = i*i; j <= 50; j += i+i)
+                {
+                    isPrime[j] = false;
+                }
+            }
+        }
+    }
+}
